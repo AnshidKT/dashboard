ad225f604704e5ab60f34f16af280421a8301196
anshidkt <anshidkt777@gmail.com>
2024-08-12 11:50:49 +0530

    p

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,54 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div  className="w-full h-screen flex flex-col bg-gray-100">
+
+<div className='w-full bg-white flex mt-10 p-2 justify-between text-black'>
+
+<div className='w-[290px]  ml-5 '>
+
+<span className='text-4xl '>Record Pages Monitor</span>
+  
+</div>
+
+
+<div className='flex justify-evenly gap-2 mr-8  bg-slate-400'>
+
+
+
+<div className='w-60 p-2 bg-zinc-50'>
+aaa
+</div>
+<div className='w-60 p-2 bg-zinc-50'>
+aaa
+</div>
+<div className='w-60 p-2 bg-zinc-50'>
+aaa
+</div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+    </div>
   )
 }
 
